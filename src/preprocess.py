39a8d37cddd0b98bb7c889186ddfb40f78a6eb68
SHALINI SAURAV <shalinisourav07@gmail.com>
2026-04-21 18:09:53 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import nltk
+import os
 from nltk.corpus import stopwords
 
 # safe stopwords load
@@ -9,6 +10,7 @@ try:
 except:
     nltk.download('stopwords')
     stop_words = set(stopwords.words('english'))
+
 
 def clean_text(text):
     text = str(text).lower()
@@ -20,6 +22,7 @@ def clean_text(text):
     
     return " ".join(words)
 
+
 def get_sentiment(rating):
     if rating >= 4:
         return "positive"
@@ -28,10 +31,15 @@ def get_sentiment(rating):
     else:
         return "negative"
 
+
 def load_and_process():
-    df = pd.read_csv("data/reviews.csv")
+    # FIXED PATH (works locally + cloud)
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    file_path = os.path.join(base_dir, "data", "reviews.csv")
+
+    df = pd.read_csv(file_path)
 
     df['clean_review'] = df['review'].apply(clean_text)
     df['sentiment'] = df['rating'].apply(get_sentiment)
 
-    return df   
+    return df
